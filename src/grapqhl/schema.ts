294fea1b38a935d868ff43d2a6c0aedb3a97288b
ailuroq <yaroslav_ovsyannikov@list.ru>
2021-04-20 02:44:32 +0300

    fix

--- src/grapqhl/schema.ts
+++ src/grapqhl/schema.ts
@@ -34,7 +34,8 @@ export const schema = buildSchema(`
         description: String,
         level: String,
         points: Int,
-        params: params
+        params: params,
+        active: Boolean
     }
     
     input TaskCreateData {
@@ -43,7 +44,8 @@ export const schema = buildSchema(`
         type: String!,
         level: String!,
         points: Int!,
-        params: params!
+        params: params!,
+        active: Boolean!
     }
 
     input params {
