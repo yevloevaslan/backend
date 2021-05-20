8fed2240ee57ab27990246122010a5e7d48e7f63
MusAushev <mustafa-aushev@mail.ru>
2021-05-20 22:50:00 +0300

    fix

--- src/grapqhl/index.ts
+++ src/grapqhl/index.ts
@@ -9,6 +9,9 @@ import { checkTokenMiddleware } from '../routes/middlewares/auth.middleware';
 import { apiErrorHandler } from '../libs/errorHandler';
 import {login} from '../controllers/AdminController';
 import Boom from 'boom';
+import { badRequest } from 'boom';
+import { createFile } from '../controllers/FileController';
+import { upload } from '../libs/upload';
 const NODE_ENV = process.env.NODE_ENV;
 
 adminApp.use(express.json({ limit: '1mb' }));
@@ -26,6 +29,31 @@ adminApp.post('/api/admin/login', async (req, res, next) => {
         next(err);
     }
 });
+
+adminApp.post('/api/admin/upload', checkTokenMiddleware('admin'),
+    async (req, res, next) => {
+        try {
+            upload().single('file')(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    }, async (req, res, next) => {
+        try {
+            if (!req.file) {
+                throw badRequest('File is missing');
+            }
+            console.log(req.file);
+            const {location: path, key} = req.file as unknown as Record<'location'|'key', string>;
+            res.send({
+                data: {
+                    path,
+                },
+            });
+            createFile({path, key});
+        } catch (err) {
+            next(err);
+        }
+    });
 
 adminApp.use('/api/admin', checkTokenMiddleware('admin'), graphqlHTTP((request, response, _graphQLParams) => ({
     schema,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,6 @@ import users from './users';
 import admins from './admins';
 import tasks from './tasks';
 import dictionaries from './dictionaries';
-import upload from './upload';
 import aboutProjects from './aboutProjects';
 
 
@@ -13,6 +12,5 @@ router.use('/users', users);
 router.use('/admins', admins);
 router.use('/tasks', tasks);
 router.use('/dictionaries', dictionaries);
-router.use('/upload', upload);
 router.use('/about_projects', aboutProjects);
 export default router;
