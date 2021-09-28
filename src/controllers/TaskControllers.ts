a1ccc95f9623e376d2552e2617d355786a50e0d2
MusAushev <mustafa-aushev@mail.ru>
2021-09-29 00:11:36 +0300

    fix

--- src/controllers/TaskControllers.ts
+++ src/controllers/TaskControllers.ts
@@ -112,7 +112,7 @@ const validateTaskNumber = async (taskData: any, id?: string) =>{
         }
     } 
 };
-const getNextTaskNumber = async (taskData: taskDataInterface<TaskParams>) =>{
+export const getNextTaskNumber = async (taskData: taskDataInterface<TaskParams>): Promise<number> =>{
     const maxNumberInType = await TaskModel.findOne({type: taskData.type}, {number: 1}).sort({number: -1});
     const nextNumber = maxNumberInType? maxNumberInType.number+1 : 1;
     console.log(nextNumber);
---
+++ src/scripts/numbersForAllTasks.ts
@@ -0,0 +1,16 @@
+import { getNextTaskNumber } from '../controllers/TaskControllers';
+import { TaskModel } from '../db/models/Task';
+
+(async () => {
+    try {
+        const tasks = await TaskModel.find();
+        for (const task of tasks) {
+            if (!task.number) {
+                task.number = await getNextTaskNumber(task);
+                await task.save();
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+})();
