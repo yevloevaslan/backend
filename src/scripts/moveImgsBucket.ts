550a080ebb8831dd034dea37b86cc33b1a460e7b
MusAushev <mustafa-aushev@mail.ru>
2022-05-25 16:06:34 +0300

    console.log

--- src/scripts/moveImgsBucket.ts
+++ src/scripts/moveImgsBucket.ts
@@ -8,18 +8,19 @@ const toBucket = 'samott';
         await db();
         for await (const doc of UserModel.find().cursor()) {
             doc.img = doc.img.replace(fromBucket, toBucket);
-            await doc.save();
+            console.log(doc.img);
         }  
         for await (const doc of TaskModel.find().cursor()) {
             if (doc.params['photos']) {
                 for (const i in doc.params['photos']) {
                     doc.params['photos'][i] = doc.params['photos'][i].replace(fromBucket, toBucket);
+                    console.log(doc.params['photos'][i]);
                 }
             }
             if (doc.params['sound']) {
                 doc.params['sound'] = doc.params['sound'].replace(fromBucket, toBucket);
+                console.log(doc.params['sound']);
             }
-            await doc.save();
         }
     } catch (err) {
         console.error(err);
