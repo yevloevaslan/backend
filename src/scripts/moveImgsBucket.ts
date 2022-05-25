d3fb92502e24ea26ec565dd1da1ef02d4ebf3afd
MusAushev <mustafa-aushev@mail.ru>
2022-05-25 16:45:49 +0300

    files

--- src/scripts/moveImgsBucket.ts
+++ src/scripts/moveImgsBucket.ts
@@ -1,11 +1,16 @@
 import db from '../db';
-import { UserModel, TaskModel } from '../db/models';
+import { UserModel, TaskModel, FileModel } from '../db/models';
 const fromBucket = 'mus-backet';
 const toBucket = 'samott';
 
 (async () => {
     try {
         await db();
+        for await (const doc of FileModel.find().cursor()) {
+            if (!doc.path) continue;
+            doc.path = doc.path.replace(fromBucket, toBucket);
+            await doc.save();
+        }   
         for await (const doc of UserModel.find().cursor()) {
             if (doc.img) {
                 doc.img = doc.img.replace(fromBucket, toBucket);
